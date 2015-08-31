e08d5a5abdb4d638a8ac3e4e0d06daac3ef36eb1
Ahmet Zorer <ahmetcj4@hotmail.com>
2015-08-31 15:22:30 +0300

    Settings Activity is implemented.

--- ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/FavoritesActivity.java
+++ ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/FavoritesActivity.java
@@ -1,5 +1,6 @@
 package com.intern.tmob.activityextreme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,28 +12,5 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_favorites, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
