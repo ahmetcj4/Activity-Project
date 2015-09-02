759b3ae4f9b8a0c3ef4f5712eb8d306b0a0dab21
Mustafa Erdogan <mustafaerdogan1994@gmail.com>
2015-09-02 13:42:58 +0300

    Backend is merged

--- ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/WallActivityFragment.java
+++ ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/WallActivityFragment.java
@@ -1,13 +1,22 @@
 package com.intern.tmob.activityextreme;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mustafa.myapplication.backend.myApi.MyApi;
+import com.example.mustafa.myapplication.backend.myApi.model.Entity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +24,7 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class WallActivityFragment extends Fragment {
-
+    private static MyApi myApiService = null;
     List<WallItem> mWallItem = new ArrayList<>();
     RecyclerView recyclerView;
     WallItemAdapter mWallItemAdapter;
@@ -43,5 +52,42 @@ public class WallActivityFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    class FetchWallTask extends AsyncTask<Context,Void, List<Entity>> {
+        Context context;
+        @Override
+        protected List<Entity> doInBackground(Context... params) {
+            if(myApiService == null){
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://absolute-disk-105007.appspot.com/_ah/api/");
+                myApiService = builder.build();
+            }
+            context = params[0];
+            List<Entity> list = new ArrayList<>();
+            try {
+                for(int i=0;i<20;i++) {
+                //TODO 20 tane cekiyor bunu ayarlariz
+                    Entity res = myApiService.fetchWall("08",i).execute();
+                    if(res!=null)
+                        list.add(res);
+                    else
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return list;
+        }
+
+        @Override
+        protected void onPostExecute(List<Entity> entities) {
+            Log.i("entities", String.valueOf(entities.size()));
+            for(Entity e : entities){
+                mWallItem.add(new WallItem(R.mipmap.ic_launcher,
+                        (String) e.getProperties().get("name"),"1 saat once","az ornek"));
+                Log.i("entities", (String) e.getProperties().get("title"));
+            }
+        }
     }
 }
