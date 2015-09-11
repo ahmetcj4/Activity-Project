43ccc293c7d232875668562e93b6eb07f78a928c
Mustafa Erdogan <mustafaerdogan1994@gmail.com>
2015-09-11 15:19:54 +0300

    Navigation Drawer is added to wall activity

--- ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/WallActivity.java
+++ ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/WallActivity.java
@@ -3,8 +3,12 @@ package com.intern.tmob.activityextreme;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -24,19 +28,41 @@ import java.io.IOException;
 
 public class WallActivity extends AppCompatActivity {
     private static MyApi myApiService = null;
-
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-        // Show menu icon
-        final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+        setupToolbar();
+        setupDrawer();
     }
 
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        if(toolbar!=null)
+            setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if(ab!=null) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Snackbar.make(findViewById(R.id.coordinator_layout),
+                        menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,16 +76,19 @@ public class WallActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }else  if (id == R.id.action_profile) {
-            Intent intent =new Intent(this, ProfileActivity.class);
-            intent.putExtra("fid",SplashActivityFragment.mProfile.getId());
-            startActivity(intent);
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if(drawerLayout!=null)
+                    drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.action_profile:
+                Intent intent =new Intent(this, ProfileActivity.class);
+                intent.putExtra("fid",SplashActivityFragment.mProfile.getId());
+                startActivity(intent);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
