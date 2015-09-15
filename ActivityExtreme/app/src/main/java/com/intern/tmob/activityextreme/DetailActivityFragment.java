1ed1acb41c6a91a74b658245f8bfc332d7427320
Ahmet Zorer <ahmetcj4@hotmail.com>
2015-09-15 14:20:20 +0300

    detail view activity is updated

--- ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/DetailActivityFragment.java
+++ ActivityExtreme/app/src/main/java/com/intern/tmob/activityextreme/DetailActivityFragment.java
@@ -1,31 +1,49 @@
 package com.intern.tmob.activityextreme;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.mustafa.myapplication.backend.myApi.MyApi;
+import com.example.mustafa.myapplication.backend.myApi.model.Entity;
+import com.example.mustafa.myapplication.backend.myApi.model.EntityCollection;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.intern.tmob.activityextreme.view.SlidingTabLayout;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
+    private static MyApi myApiService = null;
     WallItem activity;
     String location;
-    LinearLayout cover;
+    ImageView cover;
     ImageView image;
     TextView name,date,detail,header;
     View rootView;
+    List<WallItem> mWallItem;
+    WallItemAdapter mWallItemAdapter;
+    String acomment,fid,time;
     public DetailActivityFragment() {
     }
 
@@ -35,30 +53,11 @@ public class DetailActivityFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         activity = (WallItem) getActivity().getIntent().getSerializableExtra("object");
         location = getActivity().getIntent().getStringExtra("location");
-        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
-        viewPager.setAdapter(new TabPagerAdapter(getContext(), getViews(container)));
-
-        SlidingTabLayout slidingTabLayout = (SlidingTabLayout) rootView.findViewById(R.id.sliding_tabs);
-        slidingTabLayout.setViewPager(viewPager);
-        ((CollapsingToolbarLayout) rootView.findViewById(R.id.detail_collapsing_toolbar_layout)).setTitle("");//activity.getheader());
+        fid = activity.getFid();
+        ((CollapsingToolbarLayout) rootView.findViewById(R.id.detail_collapsing_toolbar_layout)).setTitle(" ");//activity.getheader());
 //invisible olsun sonra animation ile visible olsun.
         findAndFill();
         return rootView;
-    }
-
-    private View[] getViews(ViewGroup container) {
-        String[] tabs = {"DETAYLAR","KATILANLAR","YORUMLAR"};
-        int[] layouts = {R.layout.fragment_detail_tab_0,R.layout.fragment_detail_tab_1,R.layout.fragment_detail_tab_2};
-        View[] views = new View[3];
-
-        for(int i = 0; i<views.length; i++){
-            views[i] = LayoutInflater.from(getContext()).inflate(layouts[i],container,false);
-            views[i].setTag(tabs[i]);
-        }
-        TextView detail =(TextView)views[0].findViewById(R.id.text_detail);
-        detail.setText(activity.getdetail());
-
-        return views;
     }
 
     private void findAndFill(){
@@ -67,7 +66,7 @@ public class DetailActivityFragment extends Fragment {
         detail = (TextView) rootView.findViewById(R.id.detail_details);
         header = (TextView) rootView.findViewById(R.id.detail_header);
         image = (ImageView) rootView.findViewById(R.id.detail_pp);
-        cover = (LinearLayout) rootView.findViewById(R.id.detail_cover);
+        cover = (ImageView) rootView.findViewById(R.id.detail_cover);
         int coverDrawable = -1;
         switch (activity.getheader().charAt(0)){
             case 'S':
@@ -89,8 +88,7 @@ public class DetailActivityFragment extends Fragment {
                 coverDrawable =  R.drawable.arac;
                 break;
         }
-       // Glide.with(getContext()).load(coverDrawable).placeholder(R.color.placeholder).into(cover);
-       // cover.setBackgroundResource(coverDrawable);
+        Glide.with(getContext()).load(coverDrawable).placeholder(R.color.placeholder).into(cover);
         Glide.with(getContext()).load(activity.getImageLink()).into(image);
         header.setText(activity.getheader());
         name.setText(activity.getname());
@@ -109,6 +107,25 @@ public class DetailActivityFragment extends Fragment {
                 openProfile();
             }
         });
+        mWallItem = new ArrayList<>();
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.detail_activity_recyclerview);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(llm);
+
+        mWallItemAdapter = new WallItemAdapter(mWallItem,R.layout.pager_comment_item);
+        recyclerView.setAdapter(mWallItemAdapter);
+
+        new GetCommentActivityTask().execute();
+        Button button = (Button) rootView.findViewById(R.id.addComment);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText comment = (EditText) rootView.findViewById(R.id.activity_comment);
+                acomment = comment.getText().toString();
+                comment.setText("");
+                new CommentActivityTask().execute();
+            }
+        });
 
     }
     private void openProfile() {
@@ -119,4 +136,99 @@ public class DetailActivityFragment extends Fragment {
         startActivity(intent);
     }
 
+    class CommentActivityTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            /*
+                fid : the guy's id who creates activity
+                date: date of activity
+                time: time of activity
+                commenterID: id of commenter
+                comment: comment message
+            */
+            if(myApiService == null){
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://absolute-disk-105007.appspot.com/_ah/api/");
+                myApiService = builder.build();
+            }
+            try {
+                Log.i("commentActivityTask", "girdi");
+                /*myApiService.commentActivity("707265706085188","2015.09.09-13:40","707265706085188","Beyler bu ikinci yorum").execute();*/
+                myApiService.commentActivity(fid,activity.getsent(),SplashActivityFragment.mProfile.getId(),acomment);
+                Log.i("commentActivityTask",fid+" " + activity.getsent()+ " " +SplashActivityFragment.mProfile.getId() + " " + acomment);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.i("commentActivityTask","Error");
+            }
+            return null;
+        }
+    }
+
+    class GetCommentActivityTask extends AsyncTask<Void,Void,List<Entity>> {
+
+        @Override
+        protected List<Entity> doInBackground(Void... params) {
+            if(myApiService == null){
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://absolute-disk-105007.appspot.com/_ah/api/");
+                myApiService = builder.build();
+            }
+            List<Entity> list = new ArrayList<>();
+
+            try {
+                EntityCollection x = myApiService.getCommentsActivity(fid, activity.getsent()).execute();
+                list = x.getItems();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return list;
+        }
+
+        @Override
+        protected void onPostExecute(List<Entity> entities) {
+            mWallItem.clear();
+            if(entities!=null)
+                for(Entity e : entities){
+                    mWallItem.add(new WallItem((String)e.getProperties().get("ppUrl"),
+                             (String)e.getProperties().get("name")+" "+(String)e.getProperties().get("surname"),
+                            " ", (String) e.getProperties().get("comment"), " ",
+                            (String) e.getProperties().get("commenterID")));
+                }
+            mWallItemAdapter.notifyDataSetChanged();
+        }
+    }
+
+
+//    class LikeUnlikeActivity extends AsyncTask<Void,Void,Void>{
+//
+//        /*
+//            fid : the guy's id who creates activity
+//            date: date of activity
+//            time: time of activity
+//            commenterID: id of commenter
+//         */
+//
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//            if(myApiService == null){
+//                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+//                        .setRootUrl("https://absolute-disk-105007.appspot.com/_ah/api/");
+//                myApiService = builder.build();
+//            }
+//            try {
+//                /*if(myApiService.isLiked("123","2015.09.09 14:14","123").execute()==null)*/
+//                if(myApiService.isLiked(fid,date + " " + time,commenterID).execute() == null)
+//                    Log.i("likeUnlike","not liked");
+//                else
+//                    Log.i("likeUnlike","liked");
+//                /*myApiService.likeUnlikeActivity("123","2015.09.09 14:14","123").execute();*/
+//                myApiService.likeUnlikeActivity(fid,date + " " + time,commenterID).execute();
+//                Log.i("likeUnlike","liked");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//    }
 }
